fde39d0657e24ff943cf8b3869b5a58e9a3bfc3f
Hibai Unzueta <hibai.unzueta@gmail.com>
2014-07-16 13:45:38 +0200

    Working version. Basic.

---
+++ llist.js
@@ -0,0 +1,61 @@
+function llist(params) {
+
+  var updateLi = function(selection) {
+        selection
+          .text(function(d) { return d.place + ' ' + d.price + '€' })
+        return selection
+      },
+      onclick = function(_) { console.log("onUpdate not defined") },
+      effectDuration = 1000
+
+  function list(selection) {
+
+    // This is because we could have more than one selection?
+    selection.each(function(data) {
+
+      // Select the ul element, if it exists.
+      var ul = d3.select(this).selectAll("ul").data([data])
+
+      // Otherwise, create the empty.
+      var ulEnter = ul.enter().append("ul")
+
+      // Update elements
+      var theul = d3.select(this).select("ul")
+          .call(updateLis)
+    })
+  }
+
+  function updateLis(selection) {
+    selection.each(function(data) {
+      var li = d3.select(this).selectAll("li")
+          .data(data, function key(d) { return d.place; })
+          
+      li.transition().delay(1000)
+          .duration(effectDuration)
+          .call(updateLi)
+
+      li.enter().append("li")
+          .style("opacity", 1e-6)
+          .call(updateLi)
+        .transition()
+          .duration(effectDuration)
+          .style("opacity", 1)
+
+      li.exit()
+        .transition()
+          .duration(effectDuration)
+          .style("opacity", 1e-6)
+          .remove()
+
+      li.order()
+    })
+  }
+
+  list.onUpdate = function(_) {
+    if (!arguments.length) return trim
+    onUpdate = _
+    return list
+  }
+
+  return list
+}
