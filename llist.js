1214d30df2de1130330c3f83c3f5201aaff3edb5
Hibai Unzueta <hibai.unzueta@gmail.com>
2014-08-13 12:15:40 +0200

    More simplifications and extracted key function

--- llist.js
+++ llist.js
@@ -4,15 +4,19 @@ if (typeof d3 === 'undefined') {
 
 d3.llist = function module() {
 
+  // But it could be, for example 'd.name'
   var defaultUpdateLi = function(_selection) {
     _selection.text(function(d) { return d.toString() })
   }
 
   var config = {
+    key: function(d) { return d.toString() }, // But it could be, for example 'd.id'
     updateLi: defaultUpdateLi,
-    onClick: function(d) { console.log("onClick not defined") },
-    effectDuration: 500
+    onClick: function(d) { console.log("onClick not defined", d) },
+    effectDuration: 200
   }
+
+  var ul // Initialise the root ul as undefined
 
   var exports = function(_selection) {
 
@@ -21,18 +25,16 @@ d3.llist = function module() {
 
       function updateLis(_selection) {
         _selection.each(function(_data) {
+
           var li = d3.select(this).selectAll("li")
-              .data(_data, function key(d) { return d.place; })
-              
-          //li.transition().duration(config.effectDuration)
-          //    .call(config.updateLi)
-          //    .style("opacity", 1)
+              .data(_data, config.key)
 
           li.enter().append("li")
               .style("opacity", 1e-6)
               .call(config.updateLi)
             .transition().duration(config.effectDuration)
               .style("opacity", 1)
+              .style("opacity", null)
 
           // exit old lis, mark them as exiting and transition out
           li.exit()
@@ -48,16 +50,17 @@ d3.llist = function module() {
               .call(config.updateLi)
             .transition().duration(config.effectDuration)
               .style("opacity", 1)
+              .style("opacity", null)
 
+          // Re-inserts elements into the document such that
+          // the document order matches the selection order
           li.order()
         })
       }
 
-      // Select the ul element, if it exists.
-      var ul = d3.select(this).selectAll("ul").data([_data])
-
-      // Otherwise, create the empty.
-      var ulEnter = ul.enter().append("ul")
+      if (!ul) // Create the root ul if it's not there yet
+        ul = d3.select(this)
+          .append('ul').classed('llist', true)
 
       // Update elements
       var theul = d3.select(this).select("ul")
@@ -65,6 +68,11 @@ d3.llist = function module() {
     })
   }
 
+  // Updates the key function, used to tell apart list elements
+  exports.key = function(_) {
+    config.key = _
+    return exports
+  }
 
   exports.updateLi = function(_) {
     config.updateLi = _
